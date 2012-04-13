026fccb33483d4378bc5bdbcdc6ace55827c3b15
Chandu Tennety <chandu.tennety@gmail.com>
2012-04-12 21:59:02 -0400

    V1: hard-coded nodes; render image on the fly

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
 source 'https://rubygems.org'
+
 gem 'sinatra', '1.3.2'
 gem 'haml', '3.1.4'
 gem 'graph', '2.5.0'
+
+group :development do
+  gem 'shotgun', '0.9'
+end
--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require 'haml'
+require 'graph'
+
+APP_ROOT = File.expand_path(File.dirname(__FILE__))
 
 get '/' do
-  'Hello World'
+  edges = [["a", "b"], ["a", "c"], ["b", "c"]]
+  graph = Graph.new "demo"
+  edges.each do |(n1, n2)|
+    graph.edge n1, n2
+  end
+  graph.save File.join(APP_ROOT, 'public/images/demo'), 'png'
+
+  haml :index
 end
+
+__END__
+
+@@ index
+%html
+  %body
+    %div.graph
+      %img{ :src => 'images/demo.png' }
