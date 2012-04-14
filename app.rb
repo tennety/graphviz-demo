d5e4a7db8b258e002b5a571d60fe9b605b225498
Chandu Tennety <chandu.tennety@gmail.com>
2012-04-13 22:18:44 -0400

    V2: hard-coded nodes, render dotx on the fly; add js dependencies but don't use them yet

--- app.rb
+++ app.rb
@@ -4,21 +4,45 @@ require 'graph'
 
 APP_ROOT = File.expand_path(File.dirname(__FILE__))
 
-get '/' do
+before do
   edges = [["a", "b"], ["a", "c"], ["b", "c"]]
-  graph = Graph.new "demo"
+  @graph = Graph.new "demo"
   edges.each do |(n1, n2)|
-    graph.edge n1, n2
+    @graph.edge n1, n2
   end
-  graph.save File.join(APP_ROOT, 'public/images/demo'), 'png'
+end
+
+get '/image' do
+  @graph.save File.join(APP_ROOT, 'public/images/demo'), 'png'
 
   haml :index
 end
 
+get '/dotx' do
+  str = IO.popen("/usr/bin/dot -Txdot", "w+")
+  str.puts @graph.to_s
+  str.close_write
+
+  haml :graph, :locals => {:dotx => str.readlines.join}
+end
+
 __END__
 
-@@ index
+@@ layout
 %html
+  %head
+    %script{:type => 'text/javascript', :src => 'public/js/prototype.js'}
+    %script{:type => 'text/javascript', :src => 'public/js/path.js'}
+    %script{:type => 'text/javascript', :src => 'public/js/canviz.js'}
+    %link{:rel => 'stylesheet', :type => 'text/css', :href => 'canviz.css'}
   %body
-    %div.graph
-      %img{ :src => 'images/demo.png' }
+    = yield
+
+@@ index
+.graph
+  %img{ :src => 'images/demo.png' }
+
+@@ graph
+#graph
+  = dotx
+#debug_output{:style => 'display: none;'}
